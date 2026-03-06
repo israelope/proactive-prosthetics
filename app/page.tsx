18dc5af10c523deed3088617a4e5ee072ab2e293
Oloyede Ola <israeloloyede002@gmail.com>
2026-03-06 11:22:15 +0100

    setting up environment

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,10 @@
-import React from "react";
+import React from 'react'
 
 const page = () => {
   return (
-    <main className="p-10 space-y-8 bg-white min-h-screen">
-      {/* 1. BRAND HEADING TEST */}
-      <h1 className="text-6xl text-center">
-        This is a Proactive Heading
-      </h1>
+    <>
+    </>
+  )
+}
 
-      {/* 2. BRAND BODY TEST */}
-      <p className="text-center text-xl text-brand-grey font-body">
-        This is the body text using the Outfit font.
-      </p>
-
-      {/* 3. BRAND COLORS TEST */}
-      <div className="flex flex-col items-center gap-4">
-        <button className="bg-brand-blue text-white px-8 py-3 rounded-lg font-heading hover:opacity-90 transition-all">
-          Book a Consultation
-        </button>
-
-        <button className="bg-brand-light text-brand-blue px-8 py-3 rounded-full font-heading border border-brand-blue">
-          Secondary Action Button
-        </button>
-        <p className="bg-brand-grey font-">This is a test</p>
-      </div>
-    </main>
-  );
-};
-
-export default page;
+export default page
